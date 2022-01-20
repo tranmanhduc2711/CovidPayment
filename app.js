a624e3e9238eb8a4af6f2ff0b3ad358d77ab6365
tranmanhduc2711 <manhduc2711@gmail.com>
2022-01-20 10:53:28 +0700

    add cert

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var jwt  = require('jsonwebtoken')
+var dotenv = require('dotenv');
+const https = require('https');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+dotenv.config();
 var app = express();
 
 // view engine setup
@@ -21,6 +26,38 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+const book = {
+  id : 1,
+  name: 'Fallen Demon',
+  type: 'book'
+};
+
+
+app.post('/login',(req, res) => {
+  const data = req.body;
+  const accessToken = jwt.sign(
+    {
+      data: "foobar",
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: 30 }
+  );
+  res.json({accessToken});
+})
+app.get("/data", authenToken, (req, res) => {
+  res.json({ status: "Success", data: book });
+});
+function authenToken(req, res,next) {
+  const authorizationHeader = req.header('Authorization');
+  const token =  authorizationHeader.split(' ')[1];
+  if(!token){
+    res.sendStatus(401);
+  }
+  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,data)=>{
+    res.json({book});
+  })
+}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
